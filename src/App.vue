4e022c86d64e942378e96c2ef6ec3b3f28e622b0
carlossxu <1156061741@qq.com>
2020-04-27 13:03:43 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+import jsonp from 'jsonp'
+export default {
+  name: 'app',
+  components: {
+
+  },
+  data(){
+    return {
+      age:30,
+      data:''
+    }
+  },
+  mounted(){
+
+  }
+}
+</script>
 
 <style>
 #app {
